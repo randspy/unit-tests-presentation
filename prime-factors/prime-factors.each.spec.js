69d92d817e5866373b2bde3ef4cd2961e7d8362e
Przemyslaw Koziel <mos.koziel@gmail.com>
2019-09-29 15:09:05 +0200

    Parameterized tests for prime factors

---
+++ prime-factors/prime-factors.each.spec.js
@@ -0,0 +1,19 @@
+const primeFactors = require('./prime-factors');
+const each = require('jest-each').default;
+
+describe('Prime factors kata', () => {
+    each` number | factors
+            ${1} | ${[]}
+            ${2} | ${[2]}
+            ${3} | ${[3]}
+            ${4} | ${[2, 2]}
+            ${5} | ${[5]}
+            ${6} | ${[2, 3]}
+            ${7} | ${[7]}
+            ${8} | ${[2, 2, 2]}
+            ${9} | ${[3, 3]}
+            ${2 * 3 * 23 * 199} | ${[2, 3, 23, 199]}
+    `.it('$number should be factorize into $factors', ({number, factors}) => {
+        expect(primeFactors(number)).toEqual(factors);
+    });
+})
